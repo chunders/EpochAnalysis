6d5c443c449fc234e5e056da539f52d5a341451c
Chris Underwood <christopherunderwood@live.com>
2018-11-06 14:19:08 +0000

    Working version that has ran on scarf

--- 2018_Epoch_1D_analysis.py
+++ 2018_Epoch_1D_analysis.py
@@ -20,8 +20,13 @@ import sdf_helper as sh
 import os
 import matplotlib.colors as colors
 
+import argparse
+
 
 def FilesInFolder(DirectoryPath, splice):
+#==============================================================================
+# Create a list of all the files that are important from the epoch sim    
+#==============================================================================
     files = os.listdir(DirectoryPath)
     shots = []
     inputDeck = []
@@ -46,34 +51,48 @@ def FilesInFolder(DirectoryPath, splice):
 
 
 class Load_1D_data():
-    def __init__(self, path):
+    def __init__(self, path, savePath):
         self.d = sh.getdata(path)
-
+        self.savePath = savePath
+        self.count = int(path[-8:-4])
+        #Create folder to save into
+        if not os.path.exists(self.savePath):
+            os.makedirs(self.savePath)
+        
     def smooth(self, y, box_pts):
         box = np.ones(box_pts)/box_pts
         y_smooth = np.convolve(y, box, mode='same')
         return y_smooth
     
     def assignDistFuncGrid(self):
+        
         self.xGrid = self.d.dist_fn_x_px_electron.grid.data[0]
         self.yGrid_Momentum = self.d.dist_fn_x_px_electron.grid.data[1]
-        self.distFncData = self.d.dist_fn_x_px_electron.data[:,:,0].T
-
+        print 'Shape of distFunc ', np.shape(self.d.dist_fn_x_px_electron.data)
+        self.distFncData = self.d.dist_fn_x_px_electron.data.T
+#        self.distFncData = self.d.dist_fn_x_px_electron.data[:,:,0].T
+        print 'Shape of distFunc ', np.shape(self.distFncData)
         
     
     def DisplayDistributionFunc_correctAxis(self):
     #Distribution Funciton display
         self.assignDistFuncGrid()
         X, Y = np.meshgrid(self.xGrid, self.yGrid_Momentum)
-        plt.pcolor(X, Y, self.distFncData)
+        plt.pcolormesh(X, Y, self.distFncData)
         plt.colorbar()
-        plt.show()
+        if not os.path.exists(self.savePath + 'DistFunc/'):
+            os.makedirs(self.savePath+ 'DistFunc/')
+        plt.savefig(self.savePath + 'DistFunc/' + 'df_axis' + str(self.count).zfill(4) + '.png')
+#        plt.show()
         
     def DisplayDistributionFunc_imshow(self):
     #Distribution Funciton display
         plt.imshow(self.d.dist_fn_x_px_electron.data[:,:,0].T)
         plt.colorbar()
-        plt.show()        
+        if not os.path.exists(self.savePath + 'DistFunc/'):
+            os.makedirs(self.savePath+ 'DistFunc/')
+        plt.savefig(self.savePath + 'DistFunc/' + 'df_imshow' + str(self.count).zfill(4) + '.png')
+#        plt.show()        
     
     def numberDensity_and_Efields(self, smoothBoxSize = 20):
     #Plot the number density and the electric field on top of each other
@@ -84,14 +103,18 @@ class Load_1D_data():
         
         grid = (self.d.Electric_Field_Ex.grid.data[0][1:] + self.d.Electric_Field_Ex.grid.data[0][:-1]) * 0.5
         
-        ax = plt.subplot()
+        fig = plt.figure()
+        ax = plt.subplot(111)
         ax.plot(grid, self.d.Electric_Field_Ex.data, label = 'X')
         ax.plot(grid, self.d.Electric_Field_Ey.data , label = 'Y')
         ax.plot(grid, self.d.Electric_Field_Ez.data, label = 'Z')
         ax.legend();
         ax1 = ax.twinx()
         ax1.plot(grid, NDSmooth)
-        plt.show()
+        if not os.path.exists(self.savePath + 'NumDen_L/'):
+            os.makedirs(self.savePath+ 'NumDen_L/')
+        plt.savefig(self.savePath + 'NumDen_L/' + 'numAndL' + str(self.count).zfill(4) + '.png')
+#        plt.show()
         
     def SpectrumFromDistFunction(self, plotting=False):
         self.assignDistFuncGrid()
@@ -99,29 +122,93 @@ class Load_1D_data():
         output = np.c_[self.yGrid_Momentum, self.sumFuncData]
         if plotting:
             plt.plot(self.yGrid_Momentum, self.sumFuncData)
-            plt.show()
+            if not os.path.exists(self.savePath + 'DistFunc/'):
+                os.makedirs(self.savePath+ 'DistFunc/')
+            plt.savefig(self.savePath + 'DistFunc/' + 'spectrum' + str(self.count).zfill(4) + '.png')
+#        plt.show()
         return output
     
     def time(self):
         return self.d.Header.values()[10]
-        
-sdf_list, simTimeSteps, inputDeck = FilesInFolder('.', [0, -4])
-momentumEvo = []
-posAndTime = []
-for sdf in sdf_list[10:110]:
-    oneD = Load_1D_data(sdf)
-#    oneD.numberDensity_and_Efields()
-#    oneD.DisplayDistributionFunc_correctAxis()
-#    oneD.DisplayDistributionFunc_imshow()
-    timeSpectrum = oneD.SpectrumFromDistFunction()
-    momentumEvo.append(timeSpectrum[:,1])
-    posAndTime.append([np.average(oneD.xGrid), oneD.time()])
+    
+if __name__ == "__main__":
+#==============================================================================
+#     Using the Parser take in the arguemnts to run with
+#==============================================================================
+    parser = argparse.ArgumentParser()                            
+    # Get the folder path, from scratch and then from home.                   
+    parser.add_argument("--folder", "-f", type=str, required=True)
+    # Plotting options    
+    parser.add_argument("--options", "-o", type=str, required=True)
 
-momentumEvo = np.array(momentumEvo)
-posAndTime = np.array(posAndTime)
+    parser.add_argument("--vmin", "-l", type=str, required=False)
+    parser.add_argument("--vmax", "-u", type=str, required=False)
+#    parser.add_argument("--append", "-a", type=str, required=False)    
+    
+    #Create Folder Paths
+    folderPath = str(parser.parse_args().folder)
+    scratchPath = '/work/scratch/scarf585/' + folderPath
+    savePath = '/home/clfg/scarf585/' + folderPath
+    
+    #Create plot options list from the arguemnts
+    plotOptions = parser.parse_args().options.split('_')
+    
+    numDensLaser = False
+    distPlot_imshow = False
+    distPlot_axis = False
+    for opt in plotOptions:
+        if opt  == 'l':
+            numDensLaser = True
+        if opt  == 'd':
+            distPlot_imshow = True
+        if opt == 'da':
+            distPlot_axis = True
 
-plt.pcolormesh(posAndTime[:,1], oneD.yGrid_Momentum, momentumEvo.T, norm = colors.LogNorm())
-plt.colorbar()
+    
+#==============================================================================
+#     Load all the data that is needed to be analysed
+#==============================================================================
+    sdf_list, simTimeSteps, inputDeck = FilesInFolder(scratchPath, [0, -4])
+    
+    momentumEvo = []
+    posAndTime = []
+    for sdf in sdf_list:
+        oneD = Load_1D_data(scratchPath + sdf, savePath)
+        if numDensLaser:
+            oneD.numberDensity_and_Efields()
+        if distPlot_imshow:
+            oneD.DisplayDistributionFunc_imshow()
+        if distPlot_axis:            
+            oneD.DisplayDistributionFunc_correctAxis()
+
+        if sdf == sdf_list[-1]:
+            #Save the last plot as it is the spectrum
+            timeSpectrum = oneD.SpectrumFromDistFunction(True)
+        else:
+            timeSpectrum = oneD.SpectrumFromDistFunction(False)
+
+        momentumEvo.append(timeSpectrum[:,1])
+        posAndTime.append([np.average(oneD.xGrid), oneD.time()])
+    
+    momentumEvo = np.array(momentumEvo)
+    posAndTime = np.array(posAndTime)
+    
+    if not os.path.exists(savePath + 'DistEvo/'):
+        os.makedirs(savePath+ 'DistEvo/')    
+   
+    np.savetxt(savePath + 'DistEvo/xAxis.txt', posAndTime)
+    np.savetxt(savePath + 'DistEvo/yMomentum.txt', oneD.yGrid_Momentum)
+    np.savetxt(savePath + 'DistEvo/momentumEvo.txt', momentumEvo.T)
+    
+    if len(inputDeck) == 1:
+        # copy the input deck so there is a record of it
+        import shutil
+        shutil.copy2(scratchPath+inputDeck[0], savePath)
+    
+    
+    plt.pcolormesh(posAndTime[:,1], oneD.yGrid_Momentum, momentumEvo.T, norm = colors.LogNorm())
+    plt.colorbar()
+    plt.savefig(savePath + 'DistEvo/momentumEvo.png')
 
     
 
