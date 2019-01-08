6342031742c9155580b7d127f571969be5bf9cbf
Chris Underwood <christopher.underwood@york.ac.uk>
2019-01-08 16:03:26 +0000

    Rewrote file

--- 2018_Epoch_distFnc_from_txt.py
+++ 2018_Epoch_distFnc_from_txt.py
@@ -31,7 +31,9 @@ import os
 import matplotlib.pyplot as plt
 #import seaborn as sns
 import matplotlib.colors as colors
-
+import sys
+sys.path.insert(0, '/Users/chrisunderwood/Documents/Python/')
+import CUnderwood_Functions as func
 
 #==============================================================================
 # A function that replicates os.walk with a max depth level
@@ -90,47 +92,75 @@ def createPlot_dist_evo(allPx_integrated, all_xaxis, yaxis, Time = True):
         plt.pcolormesh(all_xaxis, yaxis,  allPx_integrated.T, norm=colors.LogNorm(), cmap = cmap, vmin = minDensityToPlot)
         xmin = all_xaxis[0]; xmax=all_xaxis[-1];
         plt.xlabel(r"Distance $(\mu m)$")
-    ymin= yaxis[0] ; ymax=yaxis[- 1]
-    xmin = all_xaxis[0]; xmax = all_xaxis[-1]
+    ymin= yaxis[0] ; 
+    ymax=yaxis[- 1]
+    xmin = all_xaxis[0]; 
+    xmax = all_xaxis[-1]
     cbar = plt.colorbar()
     cbar.set_label("Density (nparticles/cell)", rotation=270)
-    plt.axis([xmin,xmax,None, ymax])
+#    plt.axis([xmin,xmax,None, ymax])
     plt.ylabel(r"Momentum ($kg.ms^{-1}$)")
     plt.show()
 
-hdrive = '/Volumes/CIDU_passport/2018_Epoch_vega_1/'
-#hdrive += 'DensScan/'
-hdrive += '1114/'
-#hdrive += '20muFS_testing_selfInjection/dens/'
 
 
-folderPaths, folderNames = listFolders(hdrive)
-logPlot = True
-plot_MeV = True
-#==============================================================================
-# Search for the set of folders to look at!
-#==============================================================================
-#Index_to_save = [i for i in xrange(len(folderNames)) if folderNames[i].endswith('FS')]
-#Index_to_save = [i for i in xrange(len(folderNames)) if folderNames[i].startswith('rho')]
 
-#folderPaths = folderPaths[Index_to_save]
-#folderNames = folderNames[Index_to_save]
-print folderNames
+def loadFiles_From_Root_Folder(rootDir):
+    fp = rootDir + 'Dist_Evo/'
+    allPx_integrated = np.loadtxt(fp +'px_vs_t.txt')
+    posAxis = np.loadtxt(fp + 'xaxis_x.txt')
+    timeAxis = np.loadtxt(fp + 'xaxis_t.txt')
+    yaxis = np.loadtxt(fp + 'yaxis_p_eV.txt') 
+    return allPx_integrated, posAxis, timeAxis, yaxis
 
-#sns.set_palette(sns.color_palette("Set1", len(folderNames)))
-#sns.set_context("talk")
-#sns.set_style('darkgrid')
+#def outputElectronSpectrum(yaxis, allPx_integrated, ylimMax = 1e10):
+#    zeroIndex = func.nearposn(yaxis, 0.)
+#    finalSpecPx = allPx_integrated[-1,:]
+#    xEJ = yaxis[zeroIndex:]
+#    ySpectrum = finalSpecPx[zeroIndex:]
+#    xMeV = func.E_Joules_to_MeV(xEJ)
+#    plt.plot(xEJ, ySpectrum)
+#    plt.ylim([1, ylimMax])
+#    plt.yscale('log')
+#    
 
-#plt.figure(figsize = (10,7))
-for fp, names in zip(folderPaths, folderNames):
-    try:
-        allPx_integrated = np.loadtxt(fp +'px_vs_t.txt')
-        posAxis = np.loadtxt(fp + 'xaxis_x.txt')
-        timeAxis = np.loadtxt(fp + 'xaxis_t.txt')
-        yaxis = np.loadtxt(fp + 'yaxis_p_eV.txt')
-        
-        print fp.split('/')[-3]
-        createPlot_dist_evo(allPx_integrated, posAxis, yaxis)
-    except:
-        print 'Error loading data from'
-        print fp.split('/')[-3] + '\n'
+
+def createElectronSpectrum(rootDir):
+    fig = plt.figure(figsize=(4,4))
+    func.setup_figure_fontAttributes(size = 16)
+    spec = np.loadtxt(rootDir + 'Dist_evo/Electron_Spectrum.txt')
+    xPx = spec[:,0]
+    zeroIndex = func.nearposn(xPx, 0.)
+    eMev = func.momentumToMeV(xPx)[zeroIndex:]
+    intensity = spec[:,1][zeroIndex:]
+    plt.plot(eMev, intensity)
+    plt.yscale('log')
+    plt.ylim([1, None])
+    plt.ylabel('Electrons')
+    plt.xlabel('Energy (MeV)')
+    func.saveFigure(rootDir + 'Dist_evo/Electron Spectrum.png')
+    
+    OneMeVandAboveIndex = func.nearposn(eMev, 1)
+    nosE_above_1MeV = np.sum(intensity[OneMeVandAboveIndex:])
+    print "Number of Electrons above 1 MeV {:.2e}".format(nosE_above_1MeV)
+    print "Charge {:.2e} C".format(nosE_above_1MeV * 1.6e-19)
+    print "Charge {:.2e} pC".format(nosE_above_1MeV * 1.6e-19 * 1e12)
+    plt.vlines(eMev[OneMeVandAboveIndex], 0, 1e10)
+    
+    
+gdrive = '/Volumes/GoogleDrive/My Drive/'
+#gdrive += '2018_Epoch_1.5mmGasJet/1127_initial_attempt/fs6_d1e25_f80_15mJ/'
+gdrive += '2018_Epoch_1.5mmGasJet/1127_initial_attempt/SG_fs6_d1e25_15mJ/'
+
+
+
+logPlot = False
+plot_MeV = False
+##sns.set_palette(sns.color_palette("Set1", len(folderNames)))
+##sns.set_context("talk")
+##sns.set_style('darkgrid')
+
+allPx_integrated, posAxis, timeAxis, yaxis = loadFiles_From_Root_Folder(gdrive)
+
+#createPlot_dist_evo(allPx_integrated, posAxis, yaxis)
+createElectronSpectrum(gdrive)
